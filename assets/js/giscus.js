28c4be9ba173c4147250c02bf7a8b1674505a6d6
kimzuni <me@kimzuni.com>
2023-02-10 22:41:44 +0900

    .

--- assets/js/giscus.js
+++ assets/js/giscus.js
@@ -18,19 +18,24 @@ window.addEventListener("message", giscus_handleMessage);
 const giscus_sendMessage = function(msg) {
 	const giscusFrame = document.querySelector("iframe.giscus-frame");
 	if (!giscusFrame) return;
-	giscusFrame.contentWindow.postMessage({ giscus: msg }, "https://giscus.app");
+	if (!giscusFrame.classList.contains("giscus-frame--loading")) {
+		giscusFrame.contentWindow.postMessage({ giscus: msg }, "https://giscus.app");
+	}
 }
 
-
-mode_toggle.addEventListener("click", function() {
-	let css = this.ariaChecked == "true" ? "dark" : "light";
+const f_giscus_theme = function(btn) {
+	let css = btn.ariaChecked == "true" ? "dark" : "light";
 	if (giscus_theme.includes("/")) {
 		css = `${giscus_theme.slice(0, giscus_theme.lastIndexOf("/"))}/giscus${css == "light" ? "" : "-dark"}.css`;
 	}
+	return css;
+};
 
+html.querySelector('#comment script[src="https://giscus.app/client.js"]').setAttribute("data-theme", f_giscus_theme(mode_toggle));
+mode_toggle.addEventListener("click", function() {
 	giscus_sendMessage({
 		setConfig: {
-			theme: css
+			theme: f_giscus_theme(this)
 		}
 	});
 });
