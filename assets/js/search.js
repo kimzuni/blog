0320fdfe853287afd16da44c0da0ae905406510c
kimzuni <me@kimzuni.com>
2025-05-27 16:17:23 +0900

    migration jekyll to vitepress
    bun/vitepress init

--- assets/js/search.js
+++
@@ -1,8 +0,0 @@
-const searchbox = html.querySelector("#search");
-
-const search_focusin = function() {
-	searchbox.classList.add("focus");
-}
-const search_focusout = function() {
-	searchbox.classList.remove("focus");
-}
--- assets/js/searchbox.js
+++
@@ -1,8 +0,0 @@
-const searchbox = html.querySelector("#searchbox");
-
-const searchbox_focusin = function() {
-	searchbox.classList.add("focus");
-}
-const searchbox_focusout = function() {
-	searchbox.classList.remove("focus");
-}
